66b5ddde43fb30a1c920b57da49eaf14a640c0fd
Andreas Koepf <andreas.koepf@xamla.com>
2022-02-21 00:06:06 +0100

    Refactor AutoClip

--- auto_clip.py
+++ auto_clip.py
@@ -5,20 +5,21 @@ import numpy as np
 
 
 class AutoClip:
+    """
+    AUTOCLIP: ADAPTIVE GRADIENT CLIPPING FOR SOURCE SEPARATION NETWORKS
+    https://arxiv.org/abs/2007.14469
+    """
     def __init__(self, percentile=10, max_keep_history=10000):
         self.grad_history = deque(maxlen=max_keep_history)
         self.percentile = percentile
         self.max_keep_history = max_keep_history
 
-    @torch.no_grad()
     def compute_grad_norm(self, model):
-        total_norm = 0
-        for p in model.parameters():
-            if p.grad is None:
-                continue
-            total_norm += torch.sum(p.data ** 2).item()
-        total_norm = total_norm ** 0.5
-        return total_norm 
+        parameters = [p for p in model.parameters() if p.grad is not None]
+        if len(parameters) == 0:
+            return 0
+        total_norm = torch.norm(torch.stack([torch.norm(p.grad.detach()).cpu() for p in parameters]))
+        return total_norm.item()
 
     def __call__(self, model):
         grad_norm = self.compute_grad_norm(model)
