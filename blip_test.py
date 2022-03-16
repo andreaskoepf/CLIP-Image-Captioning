1872acf1f183852ebe03cacf3234ff8e1a32bf0b
Andreas Koepf <andreas.koepf@xamla.com>
2022-03-16 10:10:14 +0100

    direct optimization tests

--- blip_test.py
+++ blip_test.py
@@ -1,3 +1,4 @@
+import math
 import readline
 from PIL import Image
 import requests
@@ -8,6 +9,8 @@ from torchvision.transforms.functional import InterpolationMode
 
 from BLIP.models.blip import blip_decoder
 from models.blip_itm import blip_itm
+
+from sampling import sample, blip_rank, load_blip_decoder
 
 
 def load_demo_image(image_size,device):
@@ -34,43 +37,68 @@ def load_blip_ranking_model(device, image_size=384):
     return blip_model
 
 
-@torch.no_grad()
-def blip_rank(device, model_blip, image_pil, text_list, image_size=384, mode="itc"):
-    similarities= []
-
-    transform = transforms.Compose([
-        transforms.Resize((image_size,image_size),interpolation=InterpolationMode.BICUBIC),
-        transforms.ToTensor(),
-        transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
-    ]) 
-    image = transform(image_pil).unsqueeze(0).to(device)   
-
-    for caption in text_list:
-        if mode == 'itm':
-            itm_output = model_blip(image, caption, match_head='itm')
-            itm_score = F.softmax(itm_output, dim=1)[:,1]
-            similarities.append(itm_score.item())
-        elif mode == 'itc':
-            itc_score = model_blip(image, caption, match_head='itc')
-            similarities.append(itc_score.item())
-        else:
-            raise RuntimeError(f'blip ranking mode "{mode}" not supported')
-
-    return similarities
-
-
 def main():
     torch.hub.set_dir('/mnt/sdb3/torch_hub')
 
     device = torch.device('cuda', 1)
+    device0 = torch.device('cuda', 0)
 
-    print('loading ranking model')
-    blip_ranking_model = load_blip_ranking_model(device)
+    print('loading decoder model')
+    blip_decoder,transform = load_blip_decoder(device0)
 
     image_size = 384
     image, raw_image = load_demo_image(image_size=image_size, device=device)
     print('loaded image:', image.shape)
 
+    top_k = 2500
+    top_p = torch.tensor(([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]), device=device0)
+    min_len = torch.tensor(([25]*8), device=device0)
+    max_len = torch.tensor(([45]*8), device=device0)
+
+    # sample a caption for the image
+    captions,p,stats = sample(
+        image.to(device0),
+        blip_decoder,
+        sample_count=min_len.size(0),
+        top_p=top_p,
+        top_k=top_k,
+        min_len=min_len,
+        max_len=max_len,
+        force_eos_log_prob=math.log(0.95),
+        prompt='a picture of ',
+        num_runs=1)
+
+
+    # get the distributions
+    print('captions',captions)
+    caption = captions[0]
+
+    image_embeds = blip_decoder.visual_encoder(image.to(device0))
+    image_embeds = image_embeds.repeat_interleave(1, dim=0)    
+    image_atts = torch.ones(image_embeds.size()[:-1],dtype=torch.long).to(device0)
+
+    text = blip_decoder.tokenizer(caption, return_tensors="pt").to(device0)
+    text.input_ids[:,0] = blip_decoder.tokenizer.bos_token_id
+
+    # outputs = blip_decoder.text_decoder.forward(
+    #         input_ids = text.input_ids,
+    #         encoder_hidden_states=image_embeds,
+    #         encoder_attention_mask=image_atts,
+    #         return_dict=True,
+    #         output_attentions=blip_decoder.text_decoder.config.output_attentions,
+    #         output_hidden_states=blip_decoder.text_decoder.config.output_hidden_states,
+    #     )
+    # logits = outputs['logits']      # [1, 4, 30524]
+    # print('logits', logits.shape)
+
+    l2 = F.one_hot(text.input_ids, 30524) #logits.shape[-1])
+    print('l2', l2.shape)
+    logits = l2.float().to(device)
+    logits[logits==1] = 5
+    logits[logits==0] = -5
+
+    print('loading ranking model')
+    blip_ranking_model = load_blip_ranking_model(device)
 
     with torch.no_grad():
         image_embeds = blip_ranking_model.visual_encoder(image)
@@ -89,14 +117,16 @@ def main():
     print('word_embeddings', word_embeddings.weight.shape)
 
 
-    seq_length = 35
-    fake_logits = torch.randn(1, seq_length, word_embeddings.weight.size(0), device=device) * 0.01
+    #seq_length = 35
+    #fake_logits = torch.randn(1, seq_length, word_embeddings.weight.size(0), device=device) * 0.01
+    seq_length = logits.size(1)
+    fake_logits = logits.detach().clone().to(device) 
     fake_logits.requires_grad = True
     attention_mask = torch.ones(1, seq_length, device=device)
 
-    optimizer = torch.optim.AdamW(params=[fake_logits], lr=0.1, weight_decay=0)
+    optimizer = torch.optim.AdamW(params=[fake_logits], lr=0.001, weight_decay=0)
 
-    tau_start = 5.0
+    tau_start = 1.0
     tau_end = 0.001
     t_max = 5000
     for i in range(t_max):
@@ -152,8 +182,9 @@ def main():
         if (i+1) % 100 == 0:
             r = p.argmax(dim=-1)[0].tolist()
             caption = tokenizer.decode(r, skip_special_tokens=False)
-            print(f'[{i}] tau: {tau:.2f}; sim: {sim.item():.4f}; itm: {itm_output[0, 1]:.4f}; {caption}')
-            sim_rank = blip_rank(device, blip_ranking_model, raw_image, [caption])
+            #print(f'[{i}] tau: {tau:.2f}; sim: {sim.item():.4f}; itm: {itm_output[0, 1]:.4f}; {caption}')
+            print(f'[{i}] tau: {tau:.2f}; sim: {sim.item():.4f}; {caption}')
+            sim_rank = blip_rank(device, blip_ranking_model, raw_image, [caption], mode='itc')
             print('sim_rank', sim_rank)
             
 
